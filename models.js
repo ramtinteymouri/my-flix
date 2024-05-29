a03c6990d4643b4c4046e1dece5fdcb8fa5c5ad1
ramtinteymouri <ramtinteymouri@gmail.com>
2024-05-29 11:25:57 -0700

    updated model with password hash and validation

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     title: { type: String, required: true },
@@ -33,6 +34,14 @@ let userSchema = mongoose.Schema({
     birthday: Date,
     favorite_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
